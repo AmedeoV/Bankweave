dfc864bf07bb5667f81f116d9c00af99b4ff5f13
vertulloa <20638889+AmedeoV@users.noreply.github.com>
2025-12-08 15:03:02 +0000

    Add migration for BalanceSnapshot UserId column

---
+++ Migrations/20251208145613_AddUserIdToBalanceSnapshot.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bankweave.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToBalanceSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "BalanceSnapshots",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BalanceSnapshots");
+        }
+    }
+}
